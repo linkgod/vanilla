7f269b0caa414c8ee69bbf4a3179c82ca1093486
皓勋 <wangxun.wx@alibaba-inc.com>
2020-07-03 12:10:40 +0800

    build src with rollup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const a = 1
+
+export default function() {
+  for (let i = 0; i < 8; i++) {
+    console.log('hello world', a);
+  }
+}
+
